58a74a04636b725d7743676d20ca8b1971f73bf9
maximum0 <maximum.zero95@gmail.com>
2025-08-17 20:57:51 +0900

    feat(baekjoon): 1919 - 애너그램 만들기

---
+++ src/main/java/org/maximum0/string/_1919_.java
@@ -0,0 +1,34 @@
+package org.maximum0.string;
+
+import java.util.Scanner;
+
+public class _1919_ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        String input2 = sc.next();
+
+        int result = solve(input, input2);
+        System.out.println(result);
+    }
+
+    public static int[] getAlphabetCount(String input) {
+        int[] count = new int[26];
+        for (int i = 0; i < input.length(); i++) {
+            count[input.charAt(i) - 'a']++;
+        }
+        return count;
+    }
+
+    public static int solve(String input, String input2) {
+        int[] count = getAlphabetCount(input);
+        int[] count2 = getAlphabetCount(input2);
+
+        int answer = 0;
+        for (int i = 0; i < 26; i++) {
+            answer += Math.abs(count[i] - count2[i]);
+        }
+
+        return answer;
+    }
+}
