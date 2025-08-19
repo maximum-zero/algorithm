2f6bb09d36436f7a9bc7e97231b29562148fd978
maximum0 <maximum.zero95@gmail.com>
2025-08-20 00:35:38 +0900

    feat(baekjoon): 13223 - 소금 폭탄

---
+++ src/main/java/org/maximum0/string/_13223_.java
@@ -0,0 +1,39 @@
+package org.maximum0.string;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class _13223_ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String currentAt = sc.nextLine();
+        String actionAt = sc.nextLine();
+
+        String result = solve(currentAt, actionAt);
+        System.out.println(result);
+    }
+
+    public static int getSecondByTime(String time) {
+        List<Integer> timeUnit = Arrays.stream(time.split(":"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        return timeUnit.get(0) * 3600 + timeUnit.get(1) * 60 + timeUnit.get(2);
+    }
+
+    public static String solve(String currentAt, String actionAt) {
+        int currentSecond = getSecondByTime(currentAt);
+        int actionSecond = getSecondByTime(actionAt);
+
+        int diff = actionSecond - currentSecond;
+        if (diff <= 0) {
+            diff += 24 * 3600;
+        }
+
+        int hour = diff / 3600;
+        int minute = diff % 3600 / 60;
+        int second = diff % 3600 % 60;
+        return String.format("%02d:%02d:%02d", hour, minute, second);
+    }
+}
