9e99f0be5d20d2d32651254ec9c87e8f152334f0
maximum0 <maximum.zero95@gmail.com>
2025-09-01 11:20:39 +0900

    feat(baekjoon): 10158 - 개미
    - 0(t) 시간 복잡도에서 0(1) 방식으로 알고리즘 설계 변경
    - Scanner 대신 BufferedReader 로 변경하여 입력 속도 단축

---
+++ src/main/java/org/maximum0/math/_10158_.java
@@ -0,0 +1,35 @@
+package org.maximum0.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _10158_ {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int p = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+
+        int t = Integer.parseInt(br.readLine());
+
+        String result = solve(w, h, p, q, t);
+        System.out.println(result);
+    }
+
+    public static String solve(int w, int h, int p, int q, int t) {
+        int currentX = (t + p) % (2 * w);
+        int currentY = (t + q) % (2 * h);
+
+        if (currentX > w) currentX = 2 * w - currentX;
+        if (currentY > h) currentY = 2 * h - currentY;
+
+        return currentX + " " + currentY;
+    }
+}
