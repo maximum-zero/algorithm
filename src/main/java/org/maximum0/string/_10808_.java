b8eca73e222fb415f2f9c1176db6d4d9746295c7
maximum0 <maximum.zero95@gmail.com>
2025-09-02 23:00:38 +0900

    feat(baekjoon): 10808 - 알파벳 개수

---
+++ src/main/java/org/maximum0/string/_10808_.java
@@ -0,0 +1,23 @@
+package org.maximum0.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _10808_ {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        char[] text = br.readLine().toCharArray();
+
+        int[] count = new int[26];
+        for (char value : text) {
+            count[value - 'a']++;
+        }
+
+        for (int c : count) {
+            System.out.print(c + " ");
+        }
+    }
+
+}
