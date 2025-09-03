660f35c19c5f86af32017a46ba38c5982a2992e1
maximum0 <maximum.zero95@gmail.com>
2025-09-03 21:57:34 +0900

    feat(baekjoon): 11655 - ROT13

---
+++ src/main/java/org/maximum0/string/_11655_.java
@@ -0,0 +1,31 @@
+package org.maximum0.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _11655_ {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        char[] text = br.readLine().toCharArray();
+
+        StringBuilder encText = new StringBuilder();
+        for (char c : text) {
+            encText.append(encRot13(c));
+        }
+
+        System.out.println(encText);
+    }
+
+    private static char encRot13(char value) {
+        final int KEY = 13;
+
+        if (value >= 'A' && value <= 'Z') {
+            return (char) ((value - 'A' + KEY) % 26 + 'A');
+        } else if (value >= 'a' && value <= 'z') {
+            return (char) ((value - 'a' + KEY) % 26 + 'a');
+        }
+        return value;
+    }
+}
