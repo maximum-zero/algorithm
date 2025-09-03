802663f79d6e08abe67a8c28f585665cc3eb952d
maximum0 <maximum.zero95@gmail.com>
2025-09-03 21:24:09 +0900

    feat(baekjoon): 10988 - 팰린드롬인지 확인하기

---
+++ src/main/java/org/maximum0/string/_10988_.java
@@ -0,0 +1,23 @@
+package org.maximum0.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _10988_ {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        char[] text = br.readLine().toCharArray();
+
+        boolean isCompare = true;
+        for (int i = 0; i < text.length / 2; i++) {
+            if (text[i] != text[text.length -1 - i]) {
+                isCompare = false;
+                break;
+            }
+        }
+
+        System.out.println(isCompare ? 1 : 0);
+    }
+}
