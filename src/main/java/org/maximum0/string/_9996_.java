8275ffad684d5212a19f7ec67fd3ef85cc7f21d9
maximum0 <maximum.zero95@gmail.com>
2025-09-04 16:26:55 +0900

    feat(baekjoon): 9996 - 한국이 그리울 땐 서버에 접속하지
    - 패턴의 총 길이와 입력값 총 길이 비교 필요
    - startsWith() 와 endsWith() 학습

---
+++ src/main/java/org/maximum0/string/_9996_.java
@@ -0,0 +1,30 @@
+package org.maximum0.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _9996_ {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int count = Integer.parseInt(br.readLine());
+        String pattern = br.readLine();
+
+        String[] splitPattern = pattern.split("\\*");
+        int patternLength = splitPattern[0].length() + splitPattern[1].length();
+
+        for (int i = 0; i < count; i++) {
+            String text = br.readLine();
+            boolean isCompare = false;
+
+            if (patternLength <= text.length()) {
+                if (text.startsWith(splitPattern[0]) && text.endsWith(splitPattern[1])) {
+                    isCompare = true;
+                }
+            }
+
+            System.out.println(isCompare ? "DA" : "NE");
+        }
+    }
+}
