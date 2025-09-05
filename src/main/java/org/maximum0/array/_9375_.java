136e8324411421bcbffda341a00ab48f0cfbeafb
maximum0 <maximum.zero95@gmail.com>
2025-09-06 00:38:07 +0900

    feat(baekjoon): 9375 - 패션왕 신해빈

---
+++ src/main/java/org/maximum0/array/_9375_.java
@@ -0,0 +1,45 @@
+package org.maximum0.array;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class _9375_ {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+
+            Map<String, Integer> fashion = new HashMap<>();
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                String name = st.nextToken();
+                String kind = st.nextToken();
+
+                if (!fashion.containsKey(kind)) {
+                    fashion.put(kind, 1);
+                } else {
+                    int count = fashion.get(kind);
+                    fashion.put(kind, count + 1);
+                }
+            }
+
+            int total = 1;
+            for (int count : fashion.values()) {
+                total *= (count + 1);
+            }
+            bw.write(total - 1 + "\n");
+        }
+        bw.flush();
+    }
+
+}
