c3fecfe45ba80ee1fa7df333a118b785561b1d33
maximum0 <maximum.zero95@gmail.com>
2025-09-05 14:13:19 +0900

    feat(baekjoon): 10989 - 수 정렬하기 3
    - 삽입정렬 시 O(N^2) 의 시간 복잡도가 발생할 가능성으로 인한 시간 초과 발생
    - 입력되는 수의 최대값이 10,000 이기에 숫자 카운트 배열을 사용하여 위를 해결가능
    - 단, 수의 최대값이 늘어나면 메모리 제한에 걸릴 수 있으니 주의가 필요

---
+++ src/main/java/org/maximum0/array/_10989_.java
@@ -0,0 +1,27 @@
+package org.maximum0.array;
+
+import java.io.*;
+
+public class _10989_ {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        final int MAX_NUMBER = 10000;
+        int N = Integer.parseInt(br.readLine());
+
+        int[] cnt = new int[MAX_NUMBER + 1];
+        for (int i = 0; i < N; i++) {
+            int value = Integer.parseInt(br.readLine());
+            cnt[value]++;
+        }
+
+        for (int i = 1; i <= MAX_NUMBER; i++) {
+            while(cnt[i]-- > 0) {
+                bw.write(i + "\n");
+            }
+        }
+
+        bw.flush();
+    }
+}
