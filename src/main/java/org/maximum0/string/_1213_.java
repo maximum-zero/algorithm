420f88e30916af3865f05956fa20301bbcb42ea3
maximum0 <maximum.zero95@gmail.com>
2025-09-07 21:38:45 +0900

    feat(baekjoon): 1213 - 팰린드롬 만들기
    - StringBuilder 의 revers() 사용을 통한 문장 구성

---
+++ src/main/java/org/maximum0/string/_1213_.java
@@ -0,0 +1,52 @@
+package org.maximum0.string;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class _1213_ {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        String name = br.readLine();
+
+        int[] alphabetCounts = new int[26];
+        for (char c : name.toCharArray()) {
+            alphabetCounts[c - 'A']++;
+        }
+
+        int oddCount = 0;
+        char middleWord = ' ';
+        for (int i = 0; i < 26; i++) {
+            if (alphabetCounts[i] % 2 == 1) {
+                oddCount++;
+                middleWord = (char)(i + 'A');
+            }
+        }
+
+        if (oddCount > 1) {
+            bw.write("I'm Sorry Hansoo");
+        } else {
+            StringBuilder result = new StringBuilder();
+            StringBuilder prefix = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                for (int j = 0; j < alphabetCounts[i] / 2; j++) {
+                    prefix.append((char) ('A' + i));
+                }
+            }
+
+            result.append(prefix);
+            if (oddCount > 0) {
+                result.append(middleWord + "");
+            }
+            result.append(prefix.reverse());
+
+            bw.write(result.toString());
+        }
+        bw.flush();
+    }
+
+}
