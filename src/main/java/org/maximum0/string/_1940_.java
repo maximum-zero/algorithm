8e2f696e297483714bf7edd35ea6f574a882049c
maximum0 <maximum.zero95@gmail.com>
2025-09-08 14:29:23 +0900

    feat(baekjoon): 1940 - 주몽
    - 투 포인터 방식 사용으로 인한 시간 복잡도 완화
      O(N^2) -> O(NlogN)

---
+++ src/main/java/org/maximum0/string/_1940_.java
@@ -0,0 +1,48 @@
+package org.maximum0.string;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _1940_ {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] numbers = new int[N];
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(numbers);
+        int count = 0;
+        int start = 0;
+        int end = N - 1;
+
+        while (start < end) {
+            int sum = numbers[start] + numbers[end];
+
+            if (sum == M) {
+                count++;
+                start++;
+                end--;
+            } else if (sum < M) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+
+        bw.write(count + "");
+        bw.flush();
+    }
+
+}
