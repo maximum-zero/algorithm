28203a0ec8d94663c4ffd24d3e31723933bbf453
maximum0 <maximum.zero95@gmail.com>
2025-09-09 19:31:56 +0900

    feat(baekjoon): 3273 - 두 수의 합
    - 해시 테이블을 이용한 풀이 방법
    - 기존 투포인트 방식에서 변경 ( O(NlogN) -> O(N) 의 시간 복잡도 )

---
+++ src/main/java/org/maximum0/array/_3273_.java
@@ -0,0 +1,33 @@
+package org.maximum0.array;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class _3273_ {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        final int MAX_NUMBER = 1000000;
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int X = Integer.parseInt(br.readLine());
+
+        boolean[] exist = new boolean[MAX_NUMBER + 1];
+        for (int i = 0; i < N; i++) {
+            int number = Integer.parseInt(st.nextToken(" "));
+            exist[number] = true;
+        }
+
+        int count = 0;
+        for (int i = 0; i <= (X - 1) / 2; i++) {
+            if (i <= MAX_NUMBER && X - 1 <= MAX_NUMBER) {
+                count += (exist[i] && exist[X - i]) ? 1 : 0;
+            }
+        }
+
+        bw.write(count + "");
+        bw.flush();
+    }
+}
