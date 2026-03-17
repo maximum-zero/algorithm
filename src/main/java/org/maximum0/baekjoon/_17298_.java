a40966ad5d51ad3b01c5abe3727b6cc9a5e39f5a
maximum0 <maximum.zero95@gmail.com>
2026-03-18 02:34:54 +0900

    feat(baekjoon): 17298 - 오큰수
    - 단조 스택 패턴을 이용한 문제 풀이

---
+++ src/main/java/org/maximum0/baekjoon/_17298_.java
@@ -0,0 +1,42 @@
+package org.maximum0.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class _17298_ {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        int[] numbers = new int[N];
+        int[] result = new int[N];
+        Arrays.fill(result, -1);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            while (!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
+                result[stack.pop()] = numbers[i];
+            }
+            stack.push(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(result[i]).append(" ");
+        }
+
+        System.out.print(sb);
+    }
+
+}
