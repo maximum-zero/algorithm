68a43db66cf98f3ec4d41eea5ba166b849d3d898
maximum0 <maximum.zero95@gmail.com>
2026-03-30 11:51:49 +0900

    feat(base): 알고리즘 풀이 Template 추가

---
+++ src/main/java/org/maximum0/base/Template.java
@@ -0,0 +1,52 @@
+package org.maximum0.base;
+
+/*
+[문제 분석]
+- 입력 크기:
+- 자료 구조:
+- 순서 중요:
+- 연속 구간:
+- 합 관련:
+- 경우의 수:
+- 음수 포함:
+- 기타 특징:
+
+[알고리즘 선택]
+- 후보
+  1.
+  2.
+  3.
+
+- 제거
+  -
+
+- 최종
+  -
+
+[사용 근거]
+-
+-
+
+[결과]
+성공 / 실패
+원인
+-
+*/
+
+/*
+[유형]
+-
+
+[핵심]
+-
+
+[주의]
+-
+*/
+public class Template {
+
+    public static void main(String[] args) throws Exception {
+        // TODO
+
+    }
+}
